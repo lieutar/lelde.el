084ea0705dba665a4dc1802f98c0fbd052861f8f
lieutar <lieutar@gmail.com>
2025-04-26 14:04:47 +0900

    Add: simplified test

--- test/rsc/elconc/lelde.el.pss/src/lelde.src.el
+++ test/rsc/elconc/lelde.el.pss/src/lelde.src.el
@@ -3,9 +3,8 @@
 (require 'lelde/META)
 (require 'lelde/project/init)   ;; cli
 (require 'lelde/project/update) ;; cli
-(require 'lelde/tinplate)       ;; cli (with tinplate)
-(require 'lelde/elconc)         ;; cli (with elconc)
+;;(require 'lelde/tinplate)       ;; cli (with tinplate)
+;;(require 'lelde/elconc)         ;; cli (with elconc)
 (require 'lelde/stmax)          ;; cli (with stmax)
-(require 'lelde/test)           ;; test helper
 ;;!end
 (provide 'lelde)
--- test/scripts/project/modules/add-all-depended-by.unit.el
+++ test/scripts/project/modules/add-all-depended-by.unit.el
@@ -10,9 +10,30 @@
                 (d :depended-by (c))))
         (sort-cb (lambda (a b) (string< (symbol-name a)(symbol-name b)))))
     (lelde/project/modules::modules-alist--add-all-depended-by mods)
-    (describe (ppp-sexp-to-string mods))
+    ;;(describe (ppp-sexp-to-string mods))
     (it "d"
       (should (equal (sort (plist-get (cdr (assq 'd mods)) :all-depended-by)
                            sort-cb)
                      (sort '(a b c)
                            sort-cb))))))
+
+(describe "case 02"
+  (let ((mods '((l :depended-by nil)
+                (P/m :depended-by (l/e S/M/x))
+                (P/U :depended-by (P/init l))
+                (S/M/x :depended-by (S/M))
+                (S/M :depended-by (S P/U))
+                (l/cli :depended-by (l/t S l/e P/U))
+                (l/e :depended-by nil) (l/init :depended-by nil)
+                (P :depended-by (l/t l/test l/rsc l/e l/cli
+                                     l/test/util S/M S/M/x P/U P/m P/init))
+                (l/rsc :depended-by (S/M P/U P/init))
+                (S :depended-by (l))
+                (l/t :depended-by (P/U))))
+        (sort-cb (lambda (a b) (string< (symbol-name a)(symbol-name b)))))
+    (lelde/project/modules::modules-alist--add-all-depended-by mods)
+    (describe (ppp-sexp-to-string mods))
+    (it "check result"
+      (should (memq 'l (plist-get (cdr (assq 'P/U mods))
+                                  :all-depended-by))))
+    ))
